691d29849ba4058a3bc03a05469428089407c9f0
Pepilix <hello@pepilix.com>
2026-08-18 11:00:38 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE = 'peptide-toolkit-v1';
+const ASSETS = ['./peptide-dosage-calculator.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      const fetchPromise = fetch(event.request).then(function(res){
+        const resClone = res.clone();
+        caches.open(CACHE).then(function(c){ c.put(event.request, resClone); });
+        return res;
+      }).catch(function(){ return cached; });
+      return cached || fetchPromise;
+    })
+  );
+});
